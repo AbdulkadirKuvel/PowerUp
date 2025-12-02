0eff4567e374715bb0c3e218d935fd6eb368feb7
Abdulkadir <Abdulkadir.kuvel@ogr.sakarya.edu.tr>
2025-12-02 10:59:11 +0300

    added roles

---
+++ Utility/Roles.cs
@@ -0,0 +1,8 @@
+namespace PowerUp.Utility;
+
+public class Roles
+{
+    public const string RoleAdmin = "Admin";
+    public const string RoleUser = "User";
+    public const string RoleTrainer = "Trainer";
+}
