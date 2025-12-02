1fbd5cb26cabba58eb0aa9113fbd76677156c0ba
Abdulkadir <Abdulkadir.kuvel@ogr.sakarya.edu.tr>
2025-12-02 10:59:38 +0300

    added main program.cs

---
+++ Program.cs
@@ -0,0 +1,71 @@
+using PowerUp.Data;
+using PowerUp.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>(options =>
+{
+    options.Password.RequireDigit = false;
+    options.Password.RequiredLength = 3;
+    options.Password.RequireNonAlphanumeric = false;
+    options.Password.RequireUppercase = false;
+    options.Password.RequireLowercase = false;
+})
+.AddEntityFrameworkStores<ApplicationDbContext>()
+.AddDefaultTokenProviders();
+
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+// Seed admin user
+using (var scope = app.Services.CreateScope())
+{
+    var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+    var userManager = scope.ServiceProvider.GetRequiredService<UserManager<ApplicationUser>>();
+
+    // Create roles
+    if (!await roleManager.RoleExistsAsync("Admin"))
+        await roleManager.CreateAsync(new IdentityRole("Admin"));
+    if (!await roleManager.RoleExistsAsync("User"))
+        await roleManager.CreateAsync(new IdentityRole("User"));
+    if (!await roleManager.RoleExistsAsync("Trainer"))
+        await roleManager.CreateAsync(new IdentityRole("Trainer"));
+
+    // Create admin user
+    var adminUser = await userManager.FindByEmailAsync("Admin@gmail.com");
+    if (adminUser == null)
+    {
+        adminUser = new ApplicationUser { UserName = "Admin@gmail.com", Email = "Admin@gmail.com" };
+        await userManager.CreateAsync(adminUser, "Sau");
+        await userManager.AddToRoleAsync(adminUser, "Admin");
+    }
+}
+
+app.Run();
